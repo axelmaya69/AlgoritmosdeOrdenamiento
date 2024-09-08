c854e6e8a53e66773ace7e1c5aa3ac4d9e13e59d
axelmaya69 <axeldbmaya96@gmail.com>
2024-09-07 22:25:13 -0600

    radix3

--- src/main/java/com/mycompany/algoritmosdeordenamiento/AlgoritmosDeOrdenamiento.java
+++ src/main/java/com/mycompany/algoritmosdeordenamiento/AlgoritmosDeOrdenamiento.java
@@ -26,7 +26,7 @@ public class AlgoritmosDeOrdenamiento {
                     
                 }
             }
-        }
+        }*/
     public void mostrar(int[] arreglo){
         int k;
         for (k=0; k<arreglo.length;k++) {
@@ -34,10 +34,10 @@ public class AlgoritmosDeOrdenamiento {
         }
         System.out.println();
     }
-     */
+     
     public void radix(int[] arreglo) {
         int x, y, j;
-        for (x = Integer.SIZE - 1; x > 0; x++) {
+        for (x = Integer.SIZE - 1; x > 0; x--) {
             int auxiliar[]= new int[arreglo.length];
             j=0;
             for (int i = 0; i < arreglo.length; i++) {
@@ -49,17 +49,22 @@ public class AlgoritmosDeOrdenamiento {
                     arreglo[i-j]=arreglo[i];
                 }
             }
-            for (int k = 0; k < 10; k++) {
-                
+            for (i=j; i<auxiliar.length;i++) {
+                auxiliar[i]=arreglo[i-j];
             }
+            arreglo=auxiliar;
         }
+        System.out.println("Ordenamiento con radix: ");
+        mostrar(arreglo);
     }
 
     public static void main(String[] args) {
-        AlgoritmosDeOrdenamiento burbuja = new AlgoritmosDeOrdenamiento();
+        //AlgoritmosDeOrdenamiento burbuja = new AlgoritmosDeOrdenamiento();
+        AlgoritmosDeOrdenamiento radix = new AlgoritmosDeOrdenamiento();
         int numeros[] = {12, 23, 43, 1, 2, 3, 54, 5, 65, 11, 5};
 
         // burbuja.burbuja(numeros);
         //burbuja.mostrar(numeros);
+        radix.radix(numeros);
     }
 }
